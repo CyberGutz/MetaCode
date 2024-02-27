ea4300c6be03be880b4deb2e78373c8775990c41
Gustavo <gutemagae@gmail.com>
2024-02-27 11:53:17 -0300

    Atualização: pegar usuário do computador pra mandar para o path certo

--- src/metacode.bat
+++ src/metacode.bat
@@ -1,14 +1,15 @@
 @echo off
 cls
+set "caminho=C:\Users\%USERNAME%\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Scripts"
 title Criador de Scripts MetaTrader5 no VSCode
 echo ^|******** Criador de Scripts MetaTrader5 no VSCode *********^|
-echo ^|                                                           ^|
+echo ^|                                                           
 set /p "id=| Digite o nome do programa: "
 set "file=%id%.mq5"
 set /p "author=| Digite o nome do autor: "
 set /p "link=| Digite o link do projeto: "
-echo ^| Criando na pasta Scripts...                               ^|
-cd C:\Users\gusta\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Scripts
+echo ^| Criando na pasta Scripts...                         
+cd %caminho%
 (
 echo //+------------------------------------------------------------------+
 echo //+------------------------------------------------------------------+
@@ -35,9 +36,14 @@ echo       Print^("Olá Mundo!"^);
 echo   ^}
 echo //+------------------------------------------------------------------+
 ) > %file%
-echo ^|                                                           ^|
-echo ^| Processo Finalizado com sucesso !!!                       ^|
-echo ^| Abrindo VSCode ....                                       ^|   
-echo ^|                                                           ^|
+echo ^|                                                           
+echo ^| Processo Finalizado com sucesso !!!    
+echo ^| Utilize o Seguinte ^path caso queira mudar o diretório do terminal para o do script:                    
+echo ^| Diretorio do arquivo: %caminho%
+echo .
+pause
+echo .
+echo ^| Abrindo VSCode ....                                       
+echo ^|                                                           
 echo ***************************************************************
 code %file%
